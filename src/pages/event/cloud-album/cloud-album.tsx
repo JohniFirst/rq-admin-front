437a8fc3c0dd7972f07fa9156eae52f956e3b819
JohniFirst <a15708295047@outlook.com>
2024-09-25 14:44:18 +0000

    feat: 增加相册部分

--- src/pages/event/cloud-album/cloud-album.tsx
+++ src/pages/event/cloud-album/cloud-album.tsx
@@ -4,10 +4,23 @@ import InfiniteScroll from 'react-infinite-scroll-component';
 import { getUploadImageList } from '@/api/api-event';
 import UploadCloudAlbum from '@/components/upload-cloud-album';
 
+import CloudAlbumStyle from './css/cloud-album.module.css'
+
+enum CloudAlbumItemType {
+  ALL,
+  ALBUM,
+}
+
 /** 云相册管理页面 */
 const CloudAlbum = () => {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState<CloudAlbumItem[]>([]);
+
+  const [defaultActive, setDefaultActive] = useState<CloudAlbumItemType>(CloudAlbumItemType.ALBUM);
+
+  const toggleDefaultActive = () => {
+    setDefaultActive(defaultActive === CloudAlbumItemType.ALL ? CloudAlbumItemType.ALBUM : CloudAlbumItemType.ALL);
+  }
 
   const loadMoreData = async () => {
     if (loading) {
@@ -29,11 +42,13 @@ const CloudAlbum = () => {
     loadMoreData();
   }, []);
 
-  return (
-    <div className='custom-container'>
-      <h2>这是云相册页面</h2>
-      <p>这是介绍</p>
-      <UploadCloudAlbum />
+  const addNewAlbum = () => {
+    console.log('新增相册')
+  }
+
+  // 所有图片
+  const AllCloudImage = () => {
+    return <>
       <div id="scrollableDiv" >
         <InfiniteScroll
           className='columns-5'
@@ -51,6 +66,44 @@ const CloudAlbum = () => {
           </Image.PreviewGroup>
         </InfiniteScroll>
       </div>
+    </>
+  }
+
+  const updateAlbumName = (e: React.ChangeEvent<HTMLParagraphElement>) => {
+    console.log(e.target.innerText)
+  }
+
+  const handleKeyboardSave = (e: React.KeyboardEvent<HTMLParagraphElement>) => {
+    if (e.key === 'Enter') {
+      const target = e.target as HTMLParagraphElement;
+
+      target.blur()
+    }
+  }
+
+  // 相册
+  const AlbumPage = () => {
+    return <ul className='grid gap-4 2xl:grid-cols-12 xl:grid-cols-8 lg:grid-cols-6 md:grid-cols-4 sm:grid-cols-3'>
+      <li className='h-36 bg-red-500'>
+        <p>这里放相册封面</p>
+        <p suppressContentEditableWarning contentEditable onBlur={updateAlbumName} onKeyUp={handleKeyboardSave}>这里是相册名字</p>
+      </li>
+      <li className='h-36 bg-red-500' onClick={addNewAlbum} onKeyUp={addNewAlbum}>新增</li>
+    </ul>
+  }
+
+  return (
+    <div className='custom-container'>
+      <h2>这是云相册页面</h2>
+      <p>这是介绍</p>
+      <UploadCloudAlbum />
+
+      <section className={CloudAlbumStyle.albumTitle}>
+        <p className={defaultActive === CloudAlbumItemType.ALL ? CloudAlbumStyle.albumTitleActive : ''} onClick={toggleDefaultActive}>所有照片</p>
+        <p className={defaultActive === CloudAlbumItemType.ALBUM ? CloudAlbumStyle.albumTitleActive : ''} onClick={toggleDefaultActive}>相册</p>
+      </section>
+
+      {defaultActive === CloudAlbumItemType.ALL ? <AllCloudImage /> : <AlbumPage />}
     </div>
   )
 }
