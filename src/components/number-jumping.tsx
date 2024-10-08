9039c00a1f868961621ce6d168cacf689df38ebb
JohniFirst <a15708295047@outlook.com>
2024-10-08 01:51:22 +0000

    chore: 使用开源依赖代替自己写的组件

--- src/components/number-jumping.tsx
+++ src/components/number-jumping.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import CountUp from 'react-countup'
 
 interface NumberJumpingProps {
 	startValue?: number
@@ -7,45 +7,8 @@ interface NumberJumpingProps {
 	className?: string
 }
 
-function NumberJumping({
-	startValue = 0,
-	endValue,
-	duration = 300,
-	className,
-}: NumberJumpingProps) {
-	const [currentValue, setCurrentValue] = useState(startValue)
-
-	const animationFrameRef = useRef<number | null>(null)
-
-	useEffect(() => {
-		let elapsedTime = 0
-
-		const animate = () => {
-			if (elapsedTime < duration) {
-				// 调整非线性的增量计算方式，实现更明显的先快后慢
-				const increment =
-					(endValue - startValue) * Math.sqrt(elapsedTime / duration)
-				setCurrentValue((prevValue) => prevValue + increment)
-				elapsedTime += 10
-				animationFrameRef.current = requestAnimationFrame(animate)
-			} else {
-				setCurrentValue(endValue)
-				if (animationFrameRef.current) {
-					cancelAnimationFrame(animationFrameRef.current)
-				}
-			}
-		}
-
-		animationFrameRef.current = requestAnimationFrame(animate)
-
-		return () => {
-			if (animationFrameRef.current) {
-				cancelAnimationFrame(animationFrameRef.current)
-			}
-		}
-	}, [startValue, endValue, duration])
-
-	return <span className={className}>{currentValue}</span>
+const NumberJumping = ({ endValue, duration }: NumberJumpingProps) => {
+	return <CountUp end={endValue} duration={duration} />
 }
 
 export default NumberJumping
