0e1a44b099a38c3654cda5ccf55c7d3521f8d39e
JohniFirst <a15708295047@outlook.com>
2024-12-19 23:09:01 +0800

    feat: 完善类型提示并更新依赖

--- src/hooks/useMouse.ts
+++ src/hooks/useMouse.ts
@@ -1,12 +1,17 @@
 import { useEffect, useState } from 'react'
 
+interface UseMousePosition {
+	x: number
+	y: number
+}
+
 /**
  * Returns the current mouse position as an object with `x` and `y` properties.
  *
  * @return {Object} An object with `x` and `y` properties representing the current mouse position.
  */
-const useMouse = () => {
-	const [position, setPosition] = useState({ x: 0, y: 0 })
+const useMouse = (): UseMousePosition => {
+	const [position, setPosition] = useState<UseMousePosition>({ x: 0, y: 0 })
 
 	const updateMousePosition = (ev: MouseEvent) => {
 		setPosition({ x: ev.clientX, y: ev.clientY })
