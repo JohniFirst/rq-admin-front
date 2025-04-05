c6f87caae8095847c9148cf8feee0d6a02b5dfa9
JohniFirst <a15708295047@outlook.com>
2025-04-05 14:18:05 +0800

    feat: 登录界面响应式适配

--- src/pages-default/login-about/login-about.tsx
+++ src/pages-default/login-about/login-about.tsx
@@ -3,10 +3,12 @@ import { Novatrix } from 'uvcanvas'
 
 export default function LoginAbout() {
 	return (
-		<section className='w-screen h-screen overflow-hidden'>
-			<Novatrix />
+		<section className='overflow-hidden p-6 flex items-center justify-center w-screen h-screen'>
+			<div className='absolute top-0 left-0 right-0 bottom-0 w-screen h-screen z-[-2]'>
+				<Novatrix />
+			</div>
 
-			<section className='w-[450px] p-[24px] absolute top-1/2 left-[300px] translate-y-[-50%] bg-white bg-opacity-80 rounded-lg'>
+			<section className='w-full bg-white bg-opacity-80 rounded-lg p-6 sm:w-3/4 md:w-[500px]'>
 				<Outlet />
 			</section>
 		</section>
