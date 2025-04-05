131981edf0b9d56860c0bae7ca9cdf7af2488123
JohniFirst <a15708295047@outlook.com>
2025-04-05 13:07:07 +0800

    feat: 更改登录背景动画

--- src/pages-default/login-about/login-about.tsx
+++ src/pages-default/login-about/login-about.tsx
@@ -1,17 +1,10 @@
 import { Outlet } from 'react-router-dom'
+import { Novatrix } from 'uvcanvas'
 
 export default function LoginAbout() {
 	return (
 		<section className='w-screen h-screen overflow-hidden'>
-			<video
-				className='position-fixed top-0 left-0 w-screen h-screen object-cover z-[-1]'
-				autoPlay
-				loop
-				muted
-			>
-				<source src='/demo.mp4' type='video/mp4' />
-				您的浏览器不支持视频标签。请升级到最新版本的浏览器。
-			</video>
+			<Novatrix />
 
 			<section className='w-[450px] p-[24px] absolute top-1/2 left-[300px] translate-y-[-50%] bg-white bg-opacity-80 rounded-lg'>
 				<Outlet />
